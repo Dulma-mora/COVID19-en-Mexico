34c25d5727fb7a487374b63f8f487749a8c84f9b
María Montero <60896899+Dulma-mora@users.noreply.github.com>
2022-06-14 23:40:06 -0500

    Update tablasdefrecuencias.R

--- Scripts/tablasdefrecuencias.R
+++ Scripts/tablasdefrecuencias.R
@@ -11,25 +11,9 @@ data <- positivos_2021[,c(-1,-2,-5,-8,-19,-20,-21)]
 head(data)
 
 
-
-
-# Boxplots ----------------------------------------------------------------
-
-Boxplots <- data %>% 
-     pivot_longer(names_to = "Categoria", values_to = "Valor", cols = c(1, 3:14)) 
-
 BP <- Boxplots[sample(1:7894263, 10000),]
 
-# ggplot(BP, aes(x = Categoria, y = Valor)) +
-#     geom_boxplot()
 
-ggplot(BP[BP$Categoria == "EDAD",], aes(x=Categoria, y=Valor)) + 
-     geom_violin(alpha = 0.5) + 
-     geom_boxplot() +
-     theme_gray() +
-     labs(
-          x=element_blank(), y=element_blank()
-     )
 
 
 summary(BP)
